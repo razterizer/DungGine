31bdc639283200701cb88bb6bd214a2db9dbbe39
Rasmus Anthin <scratty@gmail.com>
2024-06-17 18:56:21 +0200

    BSPTree.h: * Fixed the bounding box inside test for the rooms so that the character actually can go out from doors from within a room.

--- BSPTree.h
+++ BSPTree.h
@@ -71,7 +71,54 @@ namespace dung
   // So fractional lengths are calculated from left to right (column-wise)
   //   or up to down (row-wise).
   
-  struct Door;
+  struct Corridor;
+  struct BSPNode;
+  
+  struct Door
+  {
+    RC pos;
+    bool is_open = false;
+    BSPNode* room = nullptr;
+    Corridor* corridor = nullptr;
+  };
+  
+  struct Corridor
+  {
+    ttl::Rectangle bb;
+    Orientation orientation = Orientation::Vertical;
+    std::array<Door*, 2> doors;
+    
+    bool is_inside_corridor(const RC& pos) const
+    {
+      switch (orientation)
+      {
+        case Orientation::Vertical:
+        {
+          bool top_door_open = doors[0]->pos.r < doors[1]->pos.r ? doors[0]->is_open : doors[1]->is_open;
+          bool bottom_door_open = doors[0]->pos.r < doors[1]->pos.r ? doors[1]->is_open : doors[0]->is_open;
+          int top_offs = top_door_open ? 0 : -1;
+          int bottom_offs = bottom_door_open ? 0 : -1;
+          if (bb.c_len < 2)
+            return bb.is_inside_offs(pos, top_offs, bottom_offs, 0, 0);
+          return bb.is_inside_offs(pos, top_offs, bottom_offs, -1, -1);
+        }
+        case Orientation::Horizontal:
+        {
+          bool left_door_open = doors[0]->pos.c < doors[1]->pos.c ? doors[0]->is_open : doors[1]->is_open;
+          bool right_door_open = doors[0]->pos.c < doors[1]->pos.c ? doors[1]->is_open : doors[0]->is_open;
+          int left_offs = left_door_open ? 0 : -1;
+          int right_offs = right_door_open ? 0 : -1;
+          if (bb.r_len < 2)
+            return bb.is_inside_offs(pos, 0, 0, left_offs, right_offs);
+          return bb.is_inside_offs(pos, -1, -1, left_offs, right_offs);
+        }
+        default: // Impossible to reach, but alas necessary with some compilers.
+          return false;
+      }
+    }
+  };
+  
+  // //////////////////////////////////////////////////////////////
 
   struct BSPNode final
   {
@@ -257,58 +304,15 @@ namespace dung
     {
       if (!is_leaf())
         return false;
+      for (auto* d : doors)
+      {
+        if (d->is_open && pos == d->pos)
+          return true;
+      }
       return bb_leaf_room.is_inside_offs(pos, -1);
     }
   };
       
-  // //////////////////////////////////////////////////////////////
-  
-  struct Corridor;
-  
-  struct Door
-  {
-    RC pos;
-    bool is_open = false;
-    BSPNode* room = nullptr;
-    Corridor* corridor = nullptr;
-  };
-  
-  struct Corridor
-  {
-    ttl::Rectangle bb;
-    Orientation orientation = Orientation::Vertical;
-    std::array<Door*, 2> doors;
-    
-    bool is_inside_corridor(const RC& pos) const
-    {
-      switch (orientation)
-      {
-        case Orientation::Vertical:
-        {
-          bool top_door_open = doors[0]->pos.r < doors[1]->pos.r ? doors[0]->is_open : doors[1]->is_open;
-          bool bottom_door_open = doors[0]->pos.r < doors[1]->pos.r ? doors[1]->is_open : doors[0]->is_open;
-          int top_offs = top_door_open ? 0 : -1;
-          int bottom_offs = bottom_door_open ? 0 : -1;
-          if (bb.c_len < 2)
-            return bb.is_inside_offs(pos, top_offs, bottom_offs, 0, 0);
-          return bb.is_inside_offs(pos, top_offs, bottom_offs, -1, -1);
-        }
-        case Orientation::Horizontal:
-        {
-          bool left_door_open = doors[0]->pos.c < doors[1]->pos.c ? doors[0]->is_open : doors[1]->is_open;
-          bool right_door_open = doors[0]->pos.c < doors[1]->pos.c ? doors[1]->is_open : doors[0]->is_open;
-          int left_offs = left_door_open ? 0 : -1;
-          int right_offs = right_door_open ? 0 : -1;
-          if (bb.r_len < 2)
-            return bb.is_inside_offs(pos, 0, 0, left_offs, right_offs);
-          return bb.is_inside_offs(pos, -1, -1, left_offs, right_offs);
-        }
-        default: // Impossible to reach, but alas necessary with some compilers.
-          return false;
-      }
-    }
-  };
-  
   // //////////////////////////////////////////////////////////////
 
   class BSPTree final
