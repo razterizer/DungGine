b42d9074bb7726fd4eb6061511c8ff640e30b0ef
Rasmus Anthin <scratty@gmail.com>
2024-06-25 13:10:57 +0200

    Items.h: * Items now has a price tag (variable "price"). The unit is FK (fantasy "kronor", (think SEK)). It is similar to SEK in value. Initial prices are randomized according to some random distribution.

--- Items.h
+++ Items.h
@@ -19,6 +19,7 @@ namespace dung
     bool fog_of_war = true;
     bool light = false;
     float weight = 0.f; // kg-ish.
+    float price = 0.f;  // SEK-ish.
     bool is_underground = false;
   };
   
@@ -30,6 +31,7 @@ namespace dung
       style.fg_color = Color::Green;
       style.bg_color = Color::Transparent2;
       weight = 0.1f;
+      price = math::roundI(20*rnd::randn_clamp(20.f, 30.f, 0.f, 1e4f))/20.f;
     }
     
     int key_id = 0;
@@ -48,6 +50,7 @@ namespace dung
       style.fg_color = Color::Yellow;
       style.bg_color = Color::Transparent2;
       weight = 0.4f;
+      price = math::roundI(20*rnd::randn_clamp(200.f, 100.f, 0.f, 1e4f))/20.f;
     }
     enum class LampType { Isotropic, Directional, NUM_ITEMS };
     LampType type = LampType::Isotropic;
