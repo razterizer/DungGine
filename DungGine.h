4aacce2dfdf5ebd2a5e1a90f82709563f4c02360
Rasmus Anthin <scratty@gmail.com>
2024-08-17 10:30:12 +0200

    DungGine.h: * Renaming arguments v_align and h_align to mb_v_align and mb_h_align in draw() function.

--- DungGine.h
+++ DungGine.h
@@ -1502,12 +1502,14 @@ namespace dung
     
     
     template<int NR, int NC>
-    void draw(SpriteHandler<NR, NC>& sh, double real_time_s, int anim_ctr, ui::VerticalAlignment v_align = ui::VerticalAlignment::CENTER, ui::HorizontalAlignment h_align = ui::HorizontalAlignment::CENTER)
+    void draw(SpriteHandler<NR, NC>& sh, double real_time_s, int anim_ctr,
+              ui::VerticalAlignment mb_v_align = ui::VerticalAlignment::CENTER,
+              ui::HorizontalAlignment mb_h_align = ui::HorizontalAlignment::CENTER)
     {
       const auto& room_corridor_map = m_environment->get_room_corridor_map();
       const auto& door_vec = m_environment->fetch_doors();
       
-      message_handler->update(sh, static_cast<float>(real_time_s), v_align, h_align);
+      message_handler->update(sh, static_cast<float>(real_time_s), mb_v_align, mb_h_align);
       
       if (m_player.show_inventory)
         draw_inventory(sh);
