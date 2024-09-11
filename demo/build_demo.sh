bdda4a44cbee77dfc852584123bc2ecb326f2560
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-09-11 20:44:48 +0200

    Update build_demo.sh
    Propagating return value from base script.

--- demo/build_demo.sh
+++ demo/build_demo.sh
@@ -5,6 +5,14 @@ additional_flags="-I../.."
 
 ../../Core/build.sh demo "$1" "${additional_flags[@]}"
 
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
+
 ### Post-Build Actions ###
 
 mkdir -p bin/fonts/
