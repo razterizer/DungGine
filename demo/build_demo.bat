4dee7d0e4f8793196c3539045d3a3abdb49035be
Rasmus Anthin <scratty@gmail.com>
2024-09-12 21:12:32 +0200

    Adding VC++ build script for demo.

---
+++ demo/build_demo.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM Set up the Visual Studio environment
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+echo Building on Windows...
+echo Building with VC++...
+
+REM Compile the demo using VC++
+cl /std:c++20 /EHsc /Fe:bin/demo.exe /Fo:bin/demo.obj .\demo.cpp /I../..
+
+REM Create necessary directories and copy resources
+if not exist bin\fonts mkdir bin\fonts
+xcopy /Y /E ..\..\..\lib\Termin8or\fonts bin\fonts
+
+if not exist bin\textures mkdir bin\textures
+xcopy /Y /E textures bin\textures
+
