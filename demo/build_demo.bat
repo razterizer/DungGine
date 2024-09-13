6b60f33f94b4dc12de788f0b7ba65bfac1e7adaf
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-09-13 21:04:06 +0200

    Update build_demo.bat
    Making sure the error code from the build will be propagated properly.

--- demo/build_demo.bat
+++ demo/build_demo.bat
@@ -23,6 +23,11 @@ echo Building with VC++...
  
 REM Compile the demo using VC++
 cl /std:c++20 /EHsc /Fe:bin/demo.exe /Fo:bin/demo.obj .\demo.cpp /I../..
+
+if %errorlevel% neq 0 (
+    echo Build failed with error code %errorlevel%.
+    exit /b %errorlevel%
+)
  
 REM Create necessary directories and copy resources
 if not exist bin\fonts mkdir bin\fonts
