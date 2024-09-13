9dd39f81ee6e806a3de8faf2f9b268e6de95eb2e
Rasmus Anthin <scratty@gmail.com>
2024-09-13 18:16:40 +0200

    build_demo.bat: * Fixing syntax errors and bug causing it to not properly load vcvarsall on all windows installations.

--- demo/build_demo.bat
+++ demo/build_demo.bat
@@ -1,17 +1,32 @@
 @echo off
-
+ 
 REM Set up the Visual Studio environment
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat" (
+    call "%ProgramFiles%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+) else (
+    echo "Error: Visual Studio environment not found!"
+    exit /b 1
+)
+ 
 echo Building on Windows...
 echo Building with VC++...
-
+ 
 REM Compile the demo using VC++
 cl /std:c++20 /EHsc /Fe:bin/demo.exe /Fo:bin/demo.obj .\demo.cpp /I../..
-
+ 
 REM Create necessary directories and copy resources
 if not exist bin\fonts mkdir bin\fonts
 xcopy /Y /E ..\..\..\lib\Termin8or\fonts bin\fonts
-
+ 
 if not exist bin\textures mkdir bin\textures
 xcopy /Y /E textures bin\textures
-
