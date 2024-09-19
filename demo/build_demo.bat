049288e3c45ee7eddca0325735d9b01850b51e75
Rasmus Anthin <scratty@gmail.com>
2024-09-19 12:59:07 +0200

    build_demo.bat: * Improved script that uses the solution directly.

--- demo/build_demo.bat
+++ demo/build_demo.bat
@@ -1,16 +1,18 @@
 call ..\..\Core\build.bat
- 
-REM Compile the demo using VC++
-cl /std:c++20 /EHsc /Fe:bin/demo.exe /Fo:bin/demo.obj .\demo.cpp /I../..
+
+cd demo.vs
+
+SET configuration="Release"
+IF "%~1" == "Debug" SET configuration="Debug"
+SET target="x64"
+IF "%~2" == "x86" SET target="x86"
+msbuild demo.vs.sln /p:Configuration=%configuration% /p:Platform=%target%
+
+cd ..
 
 if %errorlevel% neq 0 (
     echo Build failed with error code %errorlevel%.
     exit /b %errorlevel%
 )
- 
-REM Create necessary directories and copy resources
-if not exist bin\fonts mkdir bin\fonts
-xcopy /Y /E ..\..\Termin8or\fonts bin\fonts
- 
-if not exist bin\textures mkdir bin\textures
-xcopy /Y /E textures bin\textures
+
+echo Build succeeded.
