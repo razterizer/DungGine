c2ee94739d3e5365b2cff29704d9974ebc1e5fdc
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-10-02 14:38:57 +0200

    Update SolarMotionPatterns.h

--- SolarMotionPatterns.h
+++ SolarMotionPatterns.h
@@ -391,6 +391,8 @@ namespace dung
           }
         case Latitude::NUM_ITEMS: // Error state
           return SolarDirection::Nadir;
+        default: // Error state too
+          return SolarDirection::Nadir;
       }
     }
   };
