b4b4ebebbb7cacf20694b9fea83f6b2b05ad5fe0
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-10-03 17:50:43 +0200

    Update DungGine.h
    Adding _i postfix to fix windows warning.

--- DungGine.h
+++ DungGine.h
@@ -850,7 +850,7 @@ namespace dung
     bool place_potions(int num_potions, bool only_place_on_dry_land)
     {
       const auto world_size = m_environment->get_world_size();
-      const int c_max_num_iters = 1e5;
+      const int c_max_num_iters = 1e5_i;
       int num_iters = 0;
       bool valid_pos = false;
       for (int pot_idx = 0; pot_idx < num_potions; ++pot_idx)
