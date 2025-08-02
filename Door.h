5f0d4b6485f33101e351ec487e84c3de10a16c35
Rasmus Anthin <scratty@gmail.com>
2025-08-03 01:46:59 +0200

    Door.h: * (De)serializing door states is_open, is_locked, fog_of_war and light.

--- Door.h
+++ Door.h
@@ -6,6 +6,7 @@
 //
 
 #pragma once
+#include "SaveGame.h"
 #include <Termin8or/RC.h>
 
 
@@ -35,6 +36,31 @@ namespace dung
     {
       return !is_door || is_open;
     };
+    
+    void serialize(std::vector<std::string>& lines) const
+    {
+      sg::write_var(lines, SG_WRITE_VAR(is_open));
+      sg::write_var(lines, SG_WRITE_VAR(is_locked));
+      sg::write_var(lines, SG_WRITE_VAR(fog_of_war));
+      sg::write_var(lines, SG_WRITE_VAR(light));
+    }
+    
+    std::vector<std::string>::iterator deserialize(std::vector<std::string>::iterator it_line_begin,
+                                                   std::vector<std::string>::iterator it_line_end)
+    {
+      for (auto it_line = it_line_begin; it_line != it_line_end; ++it_line)
+      {
+        if (sg::read_var(&it_line, SG_READ_VAR(is_open))) {}
+        else if (sg::read_var(&it_line, SG_READ_VAR(is_locked))) {}
+        else if (sg::read_var(&it_line, SG_READ_VAR(fog_of_war))) {}
+        else if (sg::read_var(&it_line, SG_READ_VAR(light)))
+        {
+          return it_line;
+        }
+      }
+      
+      return it_line_end;
+    }
   };
 
 }
